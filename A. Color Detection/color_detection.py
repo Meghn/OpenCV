72533148d9e9609dd6fe23fb0a8438dbcc6382db
Meghna Pusala <meghana.pusala@gmail.com>
2024-07-27 01:34:40 +0400

    Color Detection Project

---
+++ A. Color Detection/color_detection.py
@@ -0,0 +1,34 @@
+import cv2
+from utils import get_limits
+from PIL import Image
+
+cap = cv2.VideoCapture(0)
+
+yellow = [0, 255, 255] # BGR color space
+
+while True:
+    ret, frame = cap.read()
+
+    hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+
+    lowerLimit, upperLimit = get_limits(yellow)
+    
+    mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+    
+    # Getting bounding box
+    mask_ = Image.fromarray(mask)
+    bbox = mask_.getbbox()
+    
+    if bbox is not None:
+        x1,y1,x2,y2 = bbox
+        cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 5)
+
+
+    cv2.imshow("frame", frame)
+
+    if cv2.waitKey(1) & 0xFF == ord(' '):
+        break
+
+cap.release()
+
+cv2.destroyAllWindows()
